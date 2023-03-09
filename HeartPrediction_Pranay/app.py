5317daea18da9c4a26ef7e7bb1100585bd1af7eb
Gopi Pranay <79460211+gopipranay1997@users.noreply.github.com>
2023-03-09 09:14:47 +0530

    Update app.py

--- HeartPrediction_Pranay/app.py
+++ HeartPrediction_Pranay/app.py
@@ -43,7 +43,7 @@ def predict():
         
 
 if __name__ == '__main__':
-	app.run(host='172.27.35.69', port=int(os.environ.get('PORT', 5501)))
+	app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 
 
